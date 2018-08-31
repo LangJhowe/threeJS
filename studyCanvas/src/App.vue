5121b9d2c9ebf0f1da1c408de847a8150725dcc7
langjhowe <670347102@qq.com>
2018-08-31 17:53:13 +0800

    init vue

---
+++ studyCanvas/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <three-container></three-container>
+  </div>
+</template>
+
+<script>
+import ThreeContainer from '@/views/three-container'
+export default {
+  name: 'App',
+  components: {
+    ThreeContainer
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ studyCanvas/src/views/three-container.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="view-container">
+    view
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    this.scene = new Scene() // 创建场景
+
+    this.camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000) // 创建摄影机
+    this.camera.position.z = 8
+
+    this.renderer = new WebGLRenderer() // 创建渲染器
+    this.renderer.setSize(window.innerWidth, window.innerHeight) // 设置画布大小
+    this.renderer.setPixelRatio(window.devicePixelRatio) // 设置像素比，针对高清屏
+    this.renderer.setClearColor(0x000000, 1) // 设置默认背景色
+
+    document.body.appendChild(this.renderer.domElement)
+  },
+  methods: {
+
+  }
+}
+</script>
